68ea736fb544b7e76dda422ab09ece9ece89f244
anamesh3arefni <ahmed.magdy.dev@gmail.com>
2019-05-02 19:32:23 +0200

    change colors in textarea

--- src/app/modules/shared/text-editor/text-editor.component.ts
+++ src/app/modules/shared/text-editor/text-editor.component.ts
@@ -10,51 +10,74 @@ export class TextEditorComponent implements OnInit {
   private items: MenuItem[];
   contextMenu = true;
   @ViewChild('criteria') criteriaRef: ElementRef;
-
-  constructor() { }
+  @ViewChild('highlights') highlightsRef: ElementRef;
+  @ViewChild('backdrop') backdropRef: ElementRef;
+  backdrop;
+  constructor() {}
 
   ngOnInit() {
     this.items = [
       {
         label: 'AND',
-        command: (event) => {
+        command: event => {
           this.paste({ e: event, criteria: this.criteriaRef });
         }
       },
       {
         label: 'OR',
-        command: (event) => {
+        command: event => {
           this.paste({ e: event, criteria: this.criteriaRef });
         }
       }
     ];
-  } 
-  openCm(e, criteriaContextMenu) {
+  }
+  openCm(e, criteriaContextMenu, text) {
     if (e.code === 'Space' && e.ctrlKey) {
       criteriaContextMenu.show(e);
     }
   }
-  paste({ e, criteria }: {
+  applyHighlights(e, text) {
+    text = text
+      .replace(/\n$/g, '\n\n')
+      .replace(/\bOR\b/g, '<span class="text-cyan">$&</span>')
+      .replace(/\bAND\b/g, '<span class="text-cyan">$&</span>');
+    this.highlightsRef.nativeElement.innerHTML = text;
+    this.scrollHandler(e);
+  }
+  scrollHandler(e) {
+    const scroll = e.target.scrollTop;
+    this.backdropRef.nativeElement.scrollTop = scroll;
+  }
+  paste({
+    e,
+    criteria
+  }: {
     e: {
       item: {
         label: any;
       };
-    }; criteria: ElementRef<any>;
+    };
+    criteria: ElementRef<any>;
   }) {
     const currentCriteria = criteria.nativeElement;
     const pasteVlaue = e.item.label;
     // criteria.nativeElement.value += e.item.label;
     criteria.nativeElement.focus();
-    if (currentCriteria.selectionStart || currentCriteria.selectionStart == '0') {
+    if (
+      currentCriteria.selectionStart ||
+      currentCriteria.selectionStart === '0'
+    ) {
       const startPos = currentCriteria.selectionStart;
       const endPos = currentCriteria.selectionEnd;
-      currentCriteria.value = currentCriteria.value.substring(0, startPos)
-        + pasteVlaue
-        + currentCriteria.value.substring(endPos, currentCriteria.value.length);
+      currentCriteria.value =
+        currentCriteria.value.substring(0, startPos) +
+        pasteVlaue +
+        currentCriteria.value.substring(endPos, currentCriteria.value.length);
       currentCriteria.selectionStart = startPos + pasteVlaue.length;
       currentCriteria.selectionEnd = startPos + pasteVlaue.length;
     } else {
       currentCriteria.value += pasteVlaue;
     }
+    this.applyHighlights(null, currentCriteria.value);
   }
 }
--- src/app/modules/shared/text-editor/text-editor.module.ts
+++ src/app/modules/shared/text-editor/text-editor.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextEditorComponent } from './text-editor.component';
 import { ContextMenuModule } from 'primeng/contextmenu';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TextEditorComponent],
-  imports: [
-    CommonModule,
-    ContextMenuModule
-  ],
-  exports:[
-    TextEditorComponent
-  ]
+  imports: [CommonModule, ContextMenuModule, InputTextareaModule, FormsModule],
+  exports: [TextEditorComponent]
 })
-export class TextEditorModule { }
+export class TextEditorModule {}
